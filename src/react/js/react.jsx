15a7380ad677bdd443f2a52c30c7963d8db0466e
NOAH MORRISON <morrison.noah@gmail.com>
2024-01-12 11:29:28 -0800

    link to supabase db

--- src/react/js/react.jsx
+++ src/react/js/react.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import ShoppingCartProvider from './components/ShoppingCartContext/ShoppingCartProvider.jsx'
+import { SupabaseProvider } from './components/SupabaseContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <ShoppingCartProvider>
-            <App />
-        </ShoppingCartProvider>
+        <SupabaseProvider>
+            <ShoppingCartProvider>
+                <App />
+            </ShoppingCartProvider>
+        </SupabaseProvider>
     </React.StrictMode>,
 )
