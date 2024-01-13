145426f3bf3835aca636037d73af5879146172c5
WRhee123 <rhee.william@yahoo.com>
2024-01-13 14:24:45 -0500

    a

--- src/react/js/components/App.jsx
+++ src/react/js/components/App.jsx
@@ -11,51 +11,49 @@ import Footer from "./Footer.jsx"
 import ShoppingCartContext from "./ShoppingCartContext/ShoppingCartContext.mjs"
 import { useState, useEffect, useContext } from "react"
 import getProductData from "../api.js"
-import { createClient } from '@supabase/supabase-js'
 import Bundle from "./Bundle.jsx"
 import Store from "./Store.jsx"
 import About from "./About.jsx"
-import { useSupabase } from './SupabaseContext.jsx';
 
 // TODO put the div containers into their own components
 // TODO Context API for all app images and text content?
 const App = () => {
-    const supabase = useSupabase();
+    
     const {
         cartDisplay,
         bundleDisplay
     } = useContext(ShoppingCartContext)
 
     const [isLoading, setIsLoading] = useState(true)
-    const [sampleProduct, setProductData] = useState({})
+    const [productData, setProductData] = useState({})
 
     useEffect(() => {
 
-        // const f = async () => {
-        //     const sampleProduct = await getProductData(1)
-        //     console.log(sampleProduct)
-        // }
-        
-        // f()
-        setProductData(sampleProduct)
-        setIsLoading(false)
+        const f = async () => {
+            const productData = await getProductData(1)
+            setProductData(productData)
+            setIsLoading(false)
+            console.log(productData)
+        }
+
+        f()
 
     }, [])
 
     if (!isLoading) {
         return (
             <>
-                {cartDisplay && <CurtainModal sampleProduct={sampleProduct} setProductData={setProductData} />}
+                {cartDisplay && <CurtainModal productData={productData} setProductData={setProductData} />}
                 <Banner />
                 <Navbar />
                 <div className='body ctn'>
                     <div className="app-content ctn">
-                        <Hero sampleProduct={sampleProduct} />
-                        <ProductDetails platforms={sampleProduct.platforms} publisher={sampleProduct.publisher} operatingSystems={sampleProduct.operating_systems} links={sampleProduct.links} rating={sampleProduct.rating} />
+                        <Hero productData={productData} />
+                        <ProductDetails platforms={productData.platforms} publisher={productData.publisher} operatingSystems={productData.operating_systems} links={productData.links} rating={productData.rating} />
                         <ProductDescription />
-                        <CriticalReception criticalReceptions={sampleProduct.critical_receptions} />
-                        <PopularCarousel sampleProduct={sampleProduct}/>
-                        <SystemReq systemRequirements={sampleProduct.system_requirements} />
+                        <CriticalReception criticalReceptions={productData.critical_receptions} />
+                        <PopularCarousel productData={productData}/>
+                        <SystemReq systemRequirements={productData.system_requirements} />
                     </div> {/* end app-content ctn */}
                 </div> {/* end body ctn */}
                 <Footer />
